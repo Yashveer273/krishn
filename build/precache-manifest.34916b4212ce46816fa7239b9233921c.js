ee90ddc423dbb7e292cdd9b344a5f34da9840d7a
Yashveer273 <Yashveerverma266@gmail.com>
2023-08-06 23:55:16 +0530

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.34916b4212ce46816fa7239b9233921c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b26d51141628b91412a",
+    "url": "/final-mern-project/static/css/main.18088237.chunk.css"
+  },
+  {
+    "revision": "9b26d51141628b91412a",
+    "url": "/final-mern-project/static/js/main.9b26d511.chunk.js"
+  },
+  {
+    "revision": "ff5d700a81e983bc1e1f",
+    "url": "/final-mern-project/static/js/1.ff5d700a.chunk.js"
+  },
+  {
+    "revision": "c1abde282e05b88bbc7f",
+    "url": "/final-mern-project/static/css/2.2ef46819.chunk.css"
+  },
+  {
+    "revision": "c1abde282e05b88bbc7f",
+    "url": "/final-mern-project/static/js/2.c1abde28.chunk.js"
+  },
+  {
+    "revision": "e215ca2140643b43ae30",
+    "url": "/final-mern-project/static/js/runtime~main.e215ca21.js"
+  },
+  {
+    "revision": "74f51c16cf48d8c1267bd279d9ab76f5",
+    "url": "/final-mern-project/index.html"
+  }
+];
